24a939b78883bb5880dc25120f7d1291df94a4d3
Shark <rynyaz@gmail.com>
2023-03-03 22:12:30 +0700

    not done

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-const formTodoElement = document.querySelector('.task-form')
-const coverLayerElement = document.querySelector('.cover-layer')
-const btnOpenFormElement = document.querySelector('.new-task')
-const iconClose = document.querySelector('.icon-close')
+const formTodoElement = document.querySelector(".task-form")
+const inputsForm = document.querySelectorAll('#input-element')
+const coverLayerElement = document.querySelector(".cover-layer")
+const btnOpenFormElement = document.querySelector(".new-task")
+const iconClose = document.querySelector(".icon-close")
 const form = document.getElementById("form");
 const validate1 = document.getElementById("input-element")
 const validate2 = document.getElementById("input-element2")
 const validate3 = document.getElementById("input-element3")
 const task = document.getElementById("task")
-const status1 = document.querySelector('.status')
+const task2 = document.getElementById("task2")
+const task3 = document.getElementById("task3")
+const status1 = document.querySelector(".status")
 const title = document.querySelector(".title")
-const countData = document.querySelector(".count")
-const todo = document.getElementById("todo")
-const doing = document.getElementById("doing")
-const finished = document.getElementById("finished")
+const countTodos = document.querySelector(".count")
+const todo = document.getElementById("Todo")
+const doing = document.getElementById("Doing")
+const finished = document.getElementById("Finished")
 const btnSubmitForm = document.querySelector(".btn-submit")
+const todosItemFolder = document.querySelector(".todos--item__folder")
 
+const statusTodo = document.querySelectorAll("input[name=status]")
+
+console.log(statusTodo)
 
 const getCurrentDate = () => {
     let today = new Date()
-    const dd = String(today.getDate()).padStart(2, '0')
-    const mm = String(today.getMonth() + 1).padStart(2, '0')
+    const dd = String(today.getDate()).padStart(2, "0")
+    const mm = String(today.getMonth() + 1).padStart(2, "0")
     const yyyy = today.getFullYear()
 
-    today = mm + '/' + dd + '/' + yyyy
+    today = mm + "/" + dd + "/" + yyyy
     return today
 }
 
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     formValidation();
 });
@@ -35,22 +42,22 @@ form.addEventListener('submit', (e) => {
 
 let formValidation = () => {
     if (validate1.value === "") {
-        validate1.style.border = '1px solid red'
+        validate1.style.border = "1px solid red"
     }
     else {
-        validate1.style.border = '1px solid green'
+        validate1.style.border = "1px solid green"
     }
     if (validate2.value === "") {
-        validate2.style.border = '1px solid red'
+        validate2.style.border = "1px solid red"
     }
     else {
-        validate2.style.border = '1px solid green'
+        validate2.style.border = "1px solid green"
     }
     if (validate3.value === "") {
-        validate3.style.border = '1px solid red'
+        validate3.style.border = "1px solid red"
     }
     else {
-        validate3.style.border = '1px solid green'
+        validate3.style.border = "1px solid green"
     }
     if (validate1.value != "" & validate2.value != "" & validate3.value != "") {
         acceptData()
@@ -65,17 +72,28 @@ let acceptData = () => {
         title: validate2.value,
         content: validate3.value,
         time: getCurrentDate(),
-        status: '',
+        state: statusTodo.value,
     });
 
     localStorage.setItem("data", JSON.stringify(data))
-
-    console.log(data)
     createTasks()
+};
+let findSelected = () => {
+    let selected = document.querySelector("input[name=status]:checked");
+    statusTodo1()
+    console.log(selected)
+}
+function statusTodo1() {
+    statusTodo.forEach(statusTodo => {
+        statusTodo.addEventListener("change",
+            findSelected);
+    })
 }
 
 let createTasks = () => {
     task.innerHTML = "";
+    task2.innerHTML = "";
+    task3.innerHTML = "";
     data.map((x, y) => {
         return (
             task.innerHTML += `
@@ -91,61 +109,15 @@ let createTasks = () => {
                 <div class="todos--component__line"></div>
                 <h6>${x.content}</h6>
                 <div class="todos--component__footer">
-                    <i class="fa-solid fa-clock"></i>
-                    <p>${x.time}</p>
+                <i class="fa-solid fa-clock"></i>
+                <p>${x.time}</p>
                 </div>
                 </div>
-            `);
+                `);
     })
 
     resetForm()
 };
-
-const TODO_TYPE = 'TODO_TYPE'
-const TODO_DOING_TYPE = 'TODO_DOING_TYPE'
-const TODO_FINISHED_TYPE = 'TODO_FINISHED_TYPE'
-
-// const render = () => {
-//     let htmlTodos = ''
-//     let htmlTodosDoing = ''
-//     let htmlTodosFinished = ''
-
-//     const countCategoryTodo = {
-//         todo: 0,
-//         doing: 0,
-//         finished: 0,
-//     }
-
-//     data.forEach((todo) => {
-//         switch (todo.status) {
-//             case TODO_TYPE: {
-//                 htmlTodos += createTasks()
-//                 countCategoryTodo.todo += 1
-//                 break
-//             }
-//             case TODO_DOING_TYPE: {
-//                 htmlTodosDoing += createTasks()
-//                 countCategoryTodo.doing += 1
-
-//                 break
-//             }
-//             case TODO_FINISHED_TYPE: {
-//                 htmlTodosFinished += createTasks()
-//                 countCategoryTodo.finished += 1
-//                 break
-//             }
-//         }
-//     })
-//     todo.innerHTML = htmlTodos
-//     doing.innerHTML = htmlTodosDoing
-//     finished.innerHTML = htmlTodosFinished
-
-//     countData.forEach((categoryTodoCount) => {
-//         categoryTodoCount.innerText = countCategoryTodo[categoryTodoCount.id]
-//     })
-// }
-// render()
-
 
 let deleteTask = (e) => {
     e.parentElement.parentElement.parentElement.remove();
@@ -169,6 +141,7 @@ let editTask = (e) => {
     validate3.value = selectedTask.children[3].innerHTML;
     hehe()
     deleteTask(e);
+
 }
 
 let resetForm = () => {
@@ -178,23 +151,23 @@ let resetForm = () => {
 };
 
 const displayForm = (status) => {
-    formTodoElement.style.display = status ? 'block' : 'none'
-    coverLayerElement.style.display = status ? 'block' : 'none'
+    formTodoElement.style.display = status ? "block" : "none"
+    coverLayerElement.style.display = status ? "block" : "none"
     status1.style.display = "none"
     // title.innerHTML = "Add new todo"
 }
 
-btnOpenFormElement.addEventListener('click', () => {
+btnOpenFormElement.addEventListener("click", () => {
     displayForm(true)
 })
 
-iconClose.addEventListener('click', () => {
+iconClose.addEventListener("click", () => {
     displayForm(false)
     // if (isEdit) {
     // isEdit = false
-    title.innerText = 'Add New Todo'
-    btnSubmitForm.innerText = 'Submit'
-    status1.style.display = 'none'
+    title.innerText = "Add New Todo"
+    btnSubmitForm.innerText = "Submit"
+    status1.style.display = "none"
     // }
 });
 (() => {
